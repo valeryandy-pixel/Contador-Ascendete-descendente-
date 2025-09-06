f065b74557fd3cb9dc4924b4f896250a7b8e0c71
Diego-1102 <74219168+Diego-1102@users.noreply.github.com>
2025-09-06 01:07:52 -0500

    Update Display_TM1638.py

--- Display_TM1638.py
+++ Display_TM1638.py
@@ -3,22 +3,22 @@ from machine import Pin
 from time import sleep
 
 # Define los pines que uses para el módulo TM1638
-stb_pin = 8    # Pin de STB
-clk_pin = 21   # Pin de CLK
-dio_pin = 20   # Pin de DIO
+stb_pin = 18    # Pin de STB
+clk_pin = 5   # Pin de CLK
+dio_pin = 19   # Pin de DIO
 
 # Crea una instancia del objeto TM1638
 tm1638 = TM1638(stb_pin, clk_pin, dio_pin)
 
 # Inicializa el display
 tm1638.init()
-print("Arranque del programa")
+print("Inicio")
 # Muestra un número en el display
-tm1638.displayNumber(12345678)
+tm1638.displayNumber(87654321)
 sleep(2)
 
 # Enciende un LED en la posición 0
-#tm1638.displayLed(2, True)
+#tm1638.displayLed(0, True)
 #sleep(2)
 
 # Limpia el display
@@ -29,18 +29,22 @@ sleep(2)
 #sleep(2)
 
 a = 0
-
+b = True
 while True:
     keys = tm1638.readKeys()
     if (keys != 0):
         for i in range(8):
             if (keys == (0x01 << i) & 0xFF):
                 print(f"Boton {7-i} presionado")
-    
-    tm1638.displayNumber(a)
-    a += 1
-    
-    sleep(0.1)
+                b = not b 
+    if (b == True):
+        tm1638.displayNumber(a)
+        a += 1
+        sleep(0.1)
+    else:
+        tm1638.displayNumber(a)
+        a = a - 1
+        sleep(0.1)
     
     
     
